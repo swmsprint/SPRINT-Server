b33b21461eece9dbef541be91fd3817d2ed1af01
JeongHoon <sherry0_@kakao.com>
2022-08-12 23:44:56 +0900

    remove every warnings on MemberServiceTest

--- src/test/java/sprint/server/service/MemberServiceTest.java
+++ src/test/java/sprint/server/service/MemberServiceTest.java
@@ -12,6 +12,7 @@ import sprint.server.repository.MemberRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,26 +24,25 @@ public class MemberServiceTest {
 
     /**
      * 회원가입 테스트
-     * @throws Exception
      */
     @Test
-    public void MemberJoinTest() throws Exception {
+    public void MemberJoinTest(){
         String testName = "TestName";
         String testName2 = "TestName2";
         /* 정상적인 요청 */
-        Member member = Member.createMember(testName, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 02, 28), 180.0f, 70f, null);
+        Member member = Member.createMember(testName, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 2, 28), 180.0f, 70f, null);
         member.setNickname(testName);
         Long saveId = memberService.join(member);
         assertEquals(testName, memberService.findById(saveId).getNickname());
 
         /* 동일 닉네임이 이미 존재하는 경우 */
-        Member member2 = Member.createMember(testName, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 02, 28), 180.0f, 70f, null);
+        Member member2 = Member.createMember(testName, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 2, 28), 180.0f, 70f, null);
         member2.setNickname(testName);
         ApiException thrown = assertThrows(ApiException.class, () -> memberService.join(member2));
         assertEquals("M0002", thrown.getErrorCode());
 
         /* 동일 이메일이 존재하는 경우 */
-        Member member3 = Member.createMember(testName2, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 02, 28), 180.0f, 70f, null);
+        Member member3 = Member.createMember(testName2, Gender.FEMALE, testName + "@sprint.com", LocalDate.of(2011, 2, 28), 180.0f, 70f, null);
         member3.setEmail("test1@sprint.com");
         ApiException thrown2 = assertThrows(ApiException.class, () -> memberService.join(member3));
         assertEquals("M0003", thrown2.getErrorCode());
@@ -50,31 +50,33 @@ public class MemberServiceTest {
 
     /**
      * 회원 정보 수정 테스트
-     * @throws Exception
      */
     @Test
-    public void ModifyMembersTest() throws Exception {
+    public void ModifyMembersTest(){
         /* 정상적인 요청 */
         ModifyMembersRequest modifyMembersRequest = new ModifyMembersRequest();
         modifyMembersRequest.setId(1L);
         modifyMembersRequest.setNickname("Modify1");
         modifyMembersRequest.setEmail("Modify@test.com");
         modifyMembersRequest.setGender(Gender.MALE);
-        modifyMembersRequest.setBirthDay(LocalDate.of(2022, 03, 11));
+        modifyMembersRequest.setBirthDay(LocalDate.of(2022, 3, 11));
         modifyMembersRequest.setHeight(166.7F);
         modifyMembersRequest.setWeight(70F);
         modifyMembersRequest.setPicture("modify@mtest.com");
         Boolean result = memberService.ModifyMembers(modifyMembersRequest);
         assertEquals(true, result);
-        Member member = memberRepository.findById(1L).get();
-        assertEquals(1L, member.getId());
-        assertEquals("Modify1", member.getNickname());
-        assertEquals("Modify@test.com", member.getEmail());
-        assertEquals(Gender.MALE, member.getGender());
-        assertEquals(LocalDate.of(2022, 03, 11), member.getBirthDay());
-        assertEquals(166.7F, member.getHeight());
-        assertEquals(70F, member.getWeight());
-        assertEquals("modify@mtest.com", member.getPicture());
+        Optional<Member> member = memberRepository.findById(1L);
+        if (member.isPresent()) {
+            assertEquals(1L, member.get().getId());
+            assertEquals("Modify1", member.get().getNickname());
+            assertEquals("Modify@test.com", member.get().getEmail());
+            assertEquals(Gender.MALE, member.get().getGender());
+            assertEquals(LocalDate.of(2022, 3, 11), member.get().getBirthDay());
+            assertEquals(166.7F, member.get().getHeight());
+            assertEquals(70F, member.get().getWeight());
+            assertEquals("modify@mtest.com", member.get().getPicture());
+        }
+
 
         /* 해당 회원이 존재하지 않을 때 */
         modifyMembersRequest.setId(0L);
@@ -84,10 +86,9 @@ public class MemberServiceTest {
 
     /**
      * 회원 정보 이름으로 검색 (LIKE) 테스트
-     * @throws Exception
      */
     @Test
-    public void findByNicknameContainingTest() throws Exception {
+    public void findByNicknameContainingTest(){
         /* 정상적인 요청 */
         List<Member> members = memberService.findByNicknameContaining("Test1");
         assertEquals(1, members.size());
@@ -99,7 +100,7 @@ public class MemberServiceTest {
      * 회원 정보 비활성화 테스트
      */
     @Test
-    public void disableMemberTest() throws Exception {
+    public void disableMemberTest(){
         /* 정상적인 요청 */
         Boolean result = memberService.disableMember(1L);
         assertEquals(true, result);
