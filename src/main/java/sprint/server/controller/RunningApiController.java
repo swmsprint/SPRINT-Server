916366d78d15444bad113c4213b651774a4e48a6
ye1ne <yewon7036@naver.com>
2022-08-20 15:56:01 +0900

    RunningApi request들에서 멤버아이디 변수명을 userId로 통일되도록 수정함

--- src/main/java/sprint/server/controller/RunningApiController.java
+++ src/main/java/sprint/server/controller/RunningApiController.java
@@ -2,15 +2,13 @@ package sprint.server.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import sprint.server.controller.datatransferobject.response.RunningInfoDTO;
+import sprint.server.controller.datatransferobject.response.*;
 import sprint.server.controller.datatransferobject.request.CreateRunningRequest;
 import sprint.server.controller.datatransferobject.request.FinishRunningRequest;
-import sprint.server.controller.datatransferobject.response.CreateRunningResponse;
-import sprint.server.controller.datatransferobject.response.FinishRunningResponse;
 import sprint.server.domain.Running;
-import sprint.server.controller.datatransferobject.response.RunningRawDataVO;
 import sprint.server.domain.member.Member;
 import sprint.server.domain.statistics.StatisticsType;
 import sprint.server.service.MemberService;
@@ -48,33 +46,34 @@ public class RunningApiController {
         return new FinishRunningResponse(running.getId(),running.getDistance(),running.getDuration(),running.getEnergy());
     }
 
-    /**러닝 정보 반환 api -> 렌더링을 위한 로우 데이터만 반환**/
-    @GetMapping("/api/running/detail")
-    public List<RunningRawDataVO> viewRunningDetail(@RequestParam(value="runningId")Long runningId,
-                                                 @RequestParam(value="memberId")Long memberId )throws JsonProcessingException{
-        Running running = runningService.findOne(runningId).get();
-        /**
-         * 아직 러닝 정보 공개 정책이 없기때문에 전부 받아서 반환해줌 -> 추후 수정 필요
-         */
-        return Arrays.asList(objectMapper.readValue(running.getRawData(), RunningRawDataVO[].class));
-    }
-
-//    /**러닝 정보 반환 api -> 일반 러닝 정보 및 로우 데이터 포함**/
+//    /**러닝 정보 반환 api -> 렌더링을 위한 로우 데이터만 반환**/
 //    @GetMapping("/api/running/detail")
-//    public ViewRunningResponse viewRunningDetail(@RequestParam(value="runningId")Long runningId,
+//    public List<RunningRawDataVO> viewRunningDetail(@RequestParam(value="runningId")Long runningId,
 //                                                 @RequestParam(value="memberId")Long memberId )throws JsonProcessingException{
 //        Running running = runningService.findOne(runningId).get();
 //        /**
 //         * 아직 러닝 정보 공개 정책이 없기때문에 전부 받아서 반환해줌 -> 추후 수정 필요
 //         */
-//
-//        return new ViewRunningResponse(running.getId(),running.getDistance(),
-//                running.getDuration(),running.getEnergy(),
-//                Arrays.asList(objectMapper.readValue(running.getRawData(), RunningRawDataVO[].class)));
+//        return Arrays.asList(objectMapper.readValue(running.getRawData(), RunningRawDataVO[].class));
 //    }
 
+    /**러닝 정보 반환 api -> 일반 러닝 정보 및 로우 데이터 포함**/
+    @GetMapping("/api/running/detail")
+    public ViewRunningResponse viewRunningDetail(@RequestParam(value="runningId")Long runningId,
+                                                 @RequestParam(value="userId")Long memberId )throws JsonProcessingException{
+        Running running = runningService.findOne(runningId).get();
+        /**
+         * 아직 러닝 정보 공개 정책이 없기때문에 전부 받아서 반환해줌 -> 추후 수정 필요
+         */
+
+        return new ViewRunningResponse(running.getId(),running.getDistance(),
+                running.getDuration(),running.getEnergy(),
+                Arrays.asList(objectMapper.readValue(running.getRawData(), RunningRawDataVO[].class)));
+    }
+
+
     @GetMapping("/api/runnings")
-    public List<RunningInfoDTO> viewRecentRunning(@RequestParam(value="memberId")Long memberId,
+    public List<RunningInfoDTO> viewRecentRunning(@RequestParam(value="userId")Long memberId,
                                                   @RequestParam(value="lastRunningId")Long lastRunningId){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         return runningService.fetchRunningPagesBy(lastRunningId,memberId).toList().stream()
